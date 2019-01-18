0657de99a8dcac90492eadde2a9811bce9dbbd02
dhansenaz <dhansen@allthingsdot.com>
2019-01-18 09:02:43 -0800

    code cleanup

--- main.js
+++ main.js
@@ -1,22 +1,12 @@
 
 $(document).ready(function() {
-    // all custom jQuery will go here
-
-
-    // $("#trigger").click(function() {
-    //     $("#demo").html("Hello, World!");
-    //     });
-
     
-
     $('#slide_button').click(function() {
         $('#slide').animate({
           width: 'toggle',display:'block'
         }, 1000, function() {
         });
       });
-
-
     
 });
 
